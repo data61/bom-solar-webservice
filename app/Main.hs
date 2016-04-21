3b244312729a07a9ee255e21ea7564d0382b41f5
Alex Mason <Alex.Mason@nicta.com.au>
2016-04-21 11:14:26 +1000

    Add configuration using configuration-utils

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,9 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 
 module Main where
@@ -24,7 +27,7 @@ import Network.Wai.Middleware.Cors (simpleCors)
 import Servant.Swagger
 import Data.Swagger
 import Data.Swagger.ParamSchema
-import Data.Swagger.Lens -- (type_)
+import Data.Swagger.Lens as S
 import Control.Lens hiding ((.=))
 import Data.HashMap.Strict ()
 
@@ -34,7 +37,8 @@ import Control.Monad.Trans.Either
 import Control.Monad.IO.Class
 
 import Data.Aeson hiding ((.=))
-import Data.Csv
+import qualified Data.Aeson as A
+import Data.Csv as Csv
 import Data.Maybe (fromJust, fromMaybe)
 
 import Data.Time.Clock
@@ -53,6 +57,38 @@ import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Concurrent.Chan
 import Control.Concurrent.MVar
+
+import           Configuration.Utils
+import           PkgInfo_bom_solar_webservice
+
+data BoMSolarConf = BoMSolarConf
+  {_bsHttpPort :: Int
+  ,_bsAccessLog :: FilePath
+  }
+$(makeLenses ''BoMSolarConf)
+
+defaultBoMSolarConf :: BoMSolarConf
+defaultBoMSolarConf = BoMSolarConf
+  {_bsHttpPort = 3003
+  ,_bsAccessLog = "access.log"
+  }
+
+instance FromJSON (BoMSolarConf -> BoMSolarConf) where
+  parseJSON = withObject "BoMSolarConf" $ \o -> id
+    <$< bsHttpPort  ..: "bsHttpPort"  % o
+    <*< bsAccessLog ..: "bsAccessLog" % o
+
+instance ToJSON BoMSolarConf where
+  toJSON a = object
+    ["bsHttpPort"  A..= _bsHttpPort a
+    ,"bsAccessLog" A..= _bsAccessLog a
+    ]
+
+pBoMSolarConf :: MParser BoMSolarConf
+pBoMSolarConf = id
+  <$< bsHttpPort  .:: option auto % short 'P' <> long "http-port"  <> metavar "PORT" <> help "HTTP Port for webservice"
+  <*< bsAccessLog .:: strOption   % short 'a' <> long "access-log" <> metavar "HTTPACCESS" <> help "HTTP Access log file"
+
 
 nlats,nlons,nhours :: Int
 nlats = 679
@@ -82,7 +118,15 @@ type BomAPIv1
   = "DNI"
       :> Capture "lat" Double :> Capture "lon" Double
       :> QueryParam "start" ISO8601 :> QueryParam "end" ISO8601
-      :> Get '[(CSV',DefaultEncodeOpts)] [DNIVal]
+      :> Get '[(CSV',DefaultEncodeOpts), JSON] [DNIVal]
+
+
+appSwagger :: Swagger
+appSwagger = toSwagger bomProxy
+  & S.info.infoTitle .~ "BoM Solar"
+  & S.info.infoVersion .~ "0.1"
+  & S.info.infoDescription ?~ "A service for retrieving Solar DNI information for a given \
+                            \location from satellite data between 1990 and 2016"
 
 iso8601Format = iso8601DateFormat $ Just "%H:%M:%S%z"
 newtype ISO8601 = ISO8601 UTCTime
@@ -116,10 +160,15 @@ instance DefaultOrdered DNIVal where
   headerOrder _ = ["UTC time","DNI"]
 instance ToNamedRecord DNIVal where
   toNamedRecord (DNIVal time val) = namedRecord
-    [ "UTC time" .= time
-    , "DNI"  .= val
+    [ "UTC time" Csv..= time
+    , "DNI"      Csv..= val
     ]
-instance ToSchema PosInt
+instance ToSchema PosInt -- where
+--   declareNamedSchema _ = pure (Just "PosInt", schema) where
+--     schema = toSchema (Proxy :: Proxy Int)
+--       & schemaMinProperties ?~ 0
+--       & schemaMaxProperties ?~ 1200
+    -- & schemaEnum ?~ ["-",Number 0, Number 1200]
 instance ToSchema ISO8601
 instance ToSchema DNIVal
 -- where
@@ -132,26 +181,33 @@ instance ToSchema DNIVal
 --           ]
 --       & schemaRequired .~ ["dniTime", "dniVal"]
 
+
+
 retriveTimeSeries
-  :: Chan (Double,Double, Maybe ISO8601, Maybe ISO8601, MVar (Either String (Vector CInt)))
+  :: Chan VecReq
   -> Double -> Double --
   -> Maybe ISO8601 -> Maybe ISO8601
   -> EitherT ServantErr IO [DNIVal]
 retriveTimeSeries ch lat lon mstart mend = do
   evec <- liftIO $ do
     ret <- newEmptyMVar
-    writeChan ch (lat,lon,mstart,mend,ret)
-    race (threadDelay 200000 >> return "Timed out") (takeMVar ret)
+    writeChan ch (VecReq lat lon mstart mend ret)
+    race (threadDelay 200000 >> return TimedOut) (takeMVar ret)
   case join evec of
-    Left err -> left err500{errReasonPhrase = errReasonPhrase err500 ++ " ("++err++")"}
-    Right vec
-      -> pure $ zipWith (coerce DNIVal)
-                    (iterate (addUTCTime hour) (fromMaybe firstHour (coerce mstart)))
-                    (map (fromIntegral :: CInt -> Int) $ SV.toList vec)
+    Left (StrErr err)           -> left err500{errReasonPhrase = errReasonPhrase err500 ++ " ("++err++")"}
+    Left (NotFound lat lon)     -> left err404{errReasonPhrase = "Lat or Lon out of bounds ("++show lat++","++show lon++")"}
+    Left (RangeErr mstart mend) -> left err400{errReasonPhrase = "Start time is before end time ("++show mstart++","++show mend++")"}
+    Left TimedOut               -> left err500{errReasonPhrase = "Request timed out"}
+    Right vec -> pure $
+      zipWith (coerce DNIVal)
+              (iterate (addUTCTime hour) $ fromMaybe firstHour (coerce mstart))
+              (map (fromIntegral :: CInt -> Int) $ SV.toList vec)
 
+mainInfo :: ProgramInfo BoMSolarConf
+mainInfo = programInfo "BoM Solar Webservice" pBoMSolarConf defaultBoMSolarConf
 
 main :: IO ()
-main = do
+main = runWithPkgInfoConfiguration mainInfo pkgInfo $ \config -> do
   enc <- liftIO $ openFile "all-DNI-reformat.nc"
   case enc of
     Left err -> fail $ show err
@@ -164,29 +220,42 @@ main = do
           ch <- newChan
 
           -- print =<< (runEitherT $ retriveTimeSeries nc band1 (cdToD lats) (cdToD lons)  (-27) 137 Nothing Nothing)
-          forkIO $ run 8080 $ simpleCors $ serve appProxy (pure (toSwagger bomProxy) :<|> retriveTimeSeries ch)
+          forkIO $ run (config ^. bsHttpPort) $ simpleCors $ serve appProxy (pure appSwagger :<|> retriveTimeSeries ch)
           vectorServer nc band1 (cdToD lats) (cdToD lons) ch
 
   where cdToD :: Vector CDouble -> Vector Double
         cdToD = coerce
 
+data VecReq = VecReq
+  {vrLat   :: !Double
+  ,vrLon   :: !Double
+  ,vrStart :: !(Maybe ISO8601)
+  ,vrEnd   :: !(Maybe ISO8601)
+  ,vrRet   :: !(MVar (Either VecReqErr (Vector CInt)))
+  }
+
+data VecReqErr
+  = NotFound Double Double
+  | RangeErr (Maybe ISO8601) (Maybe ISO8601)
+  | StrErr String
+  | TimedOut
 
 vectorServer :: NcInfo NcRead -> NcVar -> Vector Double -> Vector Double
-  -> Chan (Double,Double,Maybe ISO8601, Maybe ISO8601,MVar (Either String (Vector CInt)))
+  -> Chan VecReq
   -> IO ()
 vectorServer nc band1 lats lons ch = forever $ do
-  (lat,lon,mstart,mend,ret) <- readChan ch
+  (VecReq lat lon mstart mend ret) <- readChan ch
   let lati = vectorIndex LT FromStart lats lat
       loni = vectorIndex LT FromStart lons lon
       sidx = maybe 0      (max 0)      (getOffset mstart)
       eidx = maybe nhours (min nhours) (getOffset mend  )
-  if eidx < sidx
-    then putMVar ret (Left $ unwords ["End time before start time, start:",show mstart,"end:",show mend])
-    else do
-      evec <- getA nc band1 [lati,loni,sidx] [1,1,eidx-sidx]
-      putMVar ret $ case evec of
-        Left err -> Left (show err)
-        Right vec -> Right vec
+  if | eidx < sidx -> putMVar ret (Left $ RangeErr mstart mend)
+     | lati == -1 || loni == -1 -> putMVar ret (Left $ NotFound lat lon)
+     | otherwise -> do
+        evec <- getA nc band1 [lati,loni,sidx] [1,1,eidx-sidx]
+        putMVar ret $ case evec of
+          Left err -> Left (StrErr $ show err)
+          Right vec -> if SV.all (== -999) vec then Left (NotFound lat lon) else Right vec
   where
     getOffset :: Maybe ISO8601 -> Maybe Int
     getOffset (Just (ISO8601 utc))
@@ -200,7 +269,6 @@ vectorServer nc band1 lats lons ch = forever $ do
 
 
 data IndexStart = FromStart | FromEnd
-
 vectorIndex :: (SV.Storable a, Ord a)
             => Ordering -> IndexStart -> SV.Vector a -> a -> Int
 vectorIndex o s v val = case (go o, s) of
